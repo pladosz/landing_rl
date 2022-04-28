3ae601f8ab9e7784fe9c02bca15a28f2b5d249c8
Meraj Mammadov <meraccos@gmail.com>
2022-04-28 07:51:43 +0000

    trials

--- my_train.py
+++ my_train.py
@@ -36,12 +36,10 @@ env = 'landing-aviary-v0'
 env = gym.make(env)
 
 
-max_step = 50
-
-errors = []
+max_step = 500
 
 state=env.reset()
-action = [0, 0, 0, 0]
+action = [0, 0, 0]
 
 red = [5, 5]
 
@@ -64,68 +62,107 @@ for step in range(max_step):
                 black = np.array([y, x])
     
 
-    
-    # Find the first red point
+    # Find the right red point
     found = False
-    for x in range(0, np.shape(im_array)[1]):
-        for y in range(9, np.shape(im_array)[0]-9):
-            if (im_array[y][x][0] > 150 and im_array[y][x][1] < 100):
-                red = np.array([y, x])
-                found = True
-                break
-            if found:
-                break
-        if found:
+    green_r = [5, 5]
+    for x in range(black[1], np.shape(im_array)[1]):
+        y = black[0]
+        if (im_array[y][x][0] > 150 and im_array[y][x][1] < 100):
+            green_r = np.array([y, x])
             break
+        # if found:
+        #     break
+
+
+    
+    # Find the left red point
+    found = False
+    green_l = [5, 5]
+    for x in range(black[1], np.shape(im_array)[1]):
+        y = black[0]
+        if (im_array[y][2*black[1] - x][0] > 150 and im_array[y][2*black[1] - x][1] < 100):
+            green_l = np.array([y, x])
+            break
+        # if found:
+        #     break
+
+    
+    # # Find the first red point
+    # found = False
+    # red = [5, 5]
+    # for x in range(0, np.shape(im_array)[1]):
+    #     for y in range(9, np.shape(im_array)[0]-9):
+    #         if (im_array[y][x][0] > 150 and im_array[y][x][1] < 100):
+    #             red = np.array([y, x])
+    #             found = True
+    #         if found:
+    #             break
+    #     if found:
+    #         break
+
+
+    
+    # # Find the first green point
+    # found = False
+    # green = [5, 5]
+    # for x in range(0, np.shape(im_array)[1]):
+    #     for y in range(9, np.shape(im_array)[0]-9):
+    #         if (im_array[y][x][0] < 10 and im_array[y][x][1] > 200 and im_array[x][x][2] < 10):
+    #             green = np.array([y, x])
+    #             found = True
+    #         if found:
+    #             break
+    #     if found:
+    #         break
+
 
 
     im_array[red[0]][red[1]] = [255, 255, 255, 255]
-    im_array[black[0]][black[1]] = [255, 255, 255, 255]
+    im_array[gren_r[0]][green_r[1]] = [255, 255, 255, 255]
+    im_array[green_l[0]][green_l[1]] = [255, 255, 255, 255]
 
     im_array[32][32] = [128, 0, 128, 255]
     im_array[15][20] = [128, 0, 128, 255]
+    im_array[21][17] = [128, 0, 128, 255]
 
 
-    e = np.zeros(4)
+    e = np.zeros(6)
     e[0:2] = black-np.array([32, 32])
     e[2:4] = red - np.array([15, 20])
-    errors.append(sum(e*e)**0.5)
-
-    
-    
+    e[4:6] = green - np.array([21, 17])
 
     im = Image.fromarray(im_array, 'RGBA')
     im.save("test_images/drone_view_{0}.png".format(step))
 
 
 
-    lamb = 0.1
+    lamb = 0.00001
     Z = 1
+
     Lx1 = np.array([[-1/Z, 0, black[1]/Z, black[0]*black[1], -(1+black[1]**2), black[0]],
                    [0, -1/Z, black[0]/Z, 1+black[0]**2, -black[0]*black[1], -black[1]]])
 
     Lx2 = np.array([[-1/Z, 0, red[1]/Z, red[0]*red[1], -(1+red[1]**2), red[0]],
                    [0, -1/Z, red[0]/Z, 1+red[0]**2, -red[0]*red[1], -red[1]]])    
 
-    
-    L_ep1 = np.linalg.pinv(Lx1)
-    L_ep2 = np.linalg.pinv(Lx2)
+    Lx3 = np.array([[-1/Z, 0, green[1]/Z, green[0]*green[1], -(1+green[1]**2), green[0]],
+                   [0, -1/Z, green[0]/Z, 1+green[0]**2, -green[0]*green[1], -green[1]]])    
 
-    L_ep = []
-    L_ep[0:6] = np.transpose(L_ep1)
-    L_ep[6:12] = np.transpose(L_ep2)
-    L_ep = np.transpose(L_ep)
+
+    L_ep = np.zeros((6, 6))
+    L_ep[0:2][:] = np.transpose(np.transpose(Lx1))
+    L_ep[2:4][:] = np.transpose(np.transpose(Lx2))
+    L_ep[4:6][:] = np.transpose(np.transpose(Lx3))
+
+
+    L_ep = np.linalg.pinv(L_ep)
 
     action = -lamb * np.dot(L_ep, e)
-    action = action[0:4]
+    action = action[0:3]
 
-    # ad_z = 1 - 1/(1+np.exp(-1 * sum(e*e)**0.5))
-    action[2] = action[2]
+    ad_z = 1 - 1/(1+np.exp(-0.01 * sum(e*e)**0.5))
+    action[2] = ad_z*(action[2] - 0.2)
 
-    action[3] = (action[0]**2+action[1]**2+action[2]**2)**(1/2)
+    #action[3] = (action[0]**2+action[1]**2+action[2]**2)**(1/2)
     print("ERROR: ", e)
     print("ACTIOOOON: ", action, "\n")
-
-
-# plt.plot(np.arange(1, max_step+1), errors)
-# plt.show()
