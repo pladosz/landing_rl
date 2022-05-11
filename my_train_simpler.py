8c80eaa6b63396a5c231b25c8898836ac3dc18cb
Meraj Mammadov <meraccos@gmail.com>
2022-05-11 10:00:30 +0000

    control

--- my_train_simpler.py
+++ my_train_simpler.py
@@ -1,8 +1,10 @@
 import gym
 import os
 import numpy as np
+from math import cos, sin
 import matplotlib.pyplot as plt
 from gym_pybullet_drones.envs.single_agent_rl.TakeoffAviary import TakeoffAviary
+from scipy.spatial.transform import Rotation
 from PIL import Image
 
 env = 'landing-aviary-v0'
@@ -25,7 +27,7 @@ v_veh = np.array([0, 0])
 v_vehicle = 0.25*np.tile(v_veh, 5)
 v_z = 0.3
 
-lamb = 0.001
+lamb = 0.02
 f =  3 ** 0.5
 
 
@@ -34,6 +36,7 @@ for step in range(max_step):
     
     next_state, reward, done, info = env.step(action)
     img = next_state.transpose(1, 2, 0)
+    # print(np.rad2deg(info))
     
     # Get the map of red area
     red_map = np.zeros(res)
@@ -57,28 +60,54 @@ for step in range(max_step):
     red_mul = np.matmul(np.vstack((red_map, red_map.T)), np.arange(0, res[1]))/area_temp
     red_m = np.int_([np.sum(red_mul[res[1]:2*res[1]]), np.sum(red_mul[0:res[1]])])
 
+    # r = info[0]
+    # p = info[1]
+    # x = red_m[1]
+    # y = red_m[0]
+    # print(red_m)
+    # red_m = (f/(-x*cos(r)*sin(p)+y*sin(r)+f*cos(r)*cos(p))) * np.array([x*cos(p)+f*sin(p),
+    #                                                                    x*sin(r)*sin(p)+y*sin(r)+f*cos(r)*cos(p)])
+    # red_m = np.int_(red_m).T
+    # red_m = [red_m[0], 64 - red_m[1]]
+    # print(red_m)
+
+
+
     # Set the desired positions
     des_m = np.int_(res/2-[1,1])
 
-    # Marking the found points
+    # Mark the found points
     img[red_m[0]][red_m[1]] = [255, 255, 255, 255]
     img[des_m[0]][des_m[1]] = [128, 0, 128, 255]
-    
-    x = red_m[1]
-    y = red_m[0]
+
+    # Define the error
     e = des_m - red_m
-    # e = np.array([e[1], e[0]])
-
-    vz = 0
-    vx = 100*((lamb*Z) - vz)*(e[1]/f) 
-    vy = 100*((lamb*Z) - vz)*(e[0]/f) 
-
-    action = [lamb*vx, -lamb*vy, vz]
-
     error = np.linalg.norm(e)
-    # ad_z = 1 - 1/(1+np.exp(-0.01 * error))
-    # action[2] = ad_z * action[2]
 
+    # Ascending velocity damping factor
+    ad_z = 1 - 1/(1+np.exp(-0.001 * error))
+    
+    # IBVS velocity components
+    vz = -0.1 * ad_z
+    vx = ((2*lamb*Z) - lamb*vz)*(e[1]/f)
+    vy = ((2*lamb*Z) - lamb*vz)*(e[0]/f)
+
+    action = [vx, -vy, vz]
+
+    # Rot from the drone to world
+    rot = Rotation.from_euler('xyz', info)
+    rot = rot.as_matrix()
+
+    action = np.matmul(action, rot)
+
+    # Test the estimated pos
+    x = x*Z/f
+    y = y*Z/f
+    pos_uav = np.array([x, y, Z])
+    pos_est = np.matmul(np.linalg.inv(rot), pos_uav) # + pos_uav
+    print('Estimated Position: ', pos_est)
+
+    # Log the results
     zs.append(Z)
     ars.append(area)
     ers.append(error)
@@ -87,6 +116,7 @@ for step in range(max_step):
     im.save("test_images/drone_view_{0}.png".format(step))
     print("Step:", step, "| Area:", area, "| Z:", "%.3f" % Z, "| Action:", "%.7f" %action[0],"%.7f" %action[1],"%.7f" %action[2], "\n")
 
+# Plot the logged results
 plt.subplot(3, 1, 1)
 plt.plot(zs, label='Z')
 plt.legend()
